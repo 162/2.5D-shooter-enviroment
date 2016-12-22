0c9c810ec2ca810f26d4312a19432bd060268887
162 <kudinov.anton96@gmail.com>
2016-12-22 19:58:53 +0300

    shared_models added

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ screen = pygame.display.set_mode(DISPLAY, flags)
 # This has to be done somewhere inside world loading
 # It`s applying keyboard mode to the only agent
 
-w.agents[0].name = '2700'
+w.agents[0].name = 'JGbot'
 #w.agents[1].name = '1100'
 #w.agents[2].name = '1600'
 w.agents[1].name = 'Random'
 
-w.agents[0].load('saved/DQN52753.h5')
-w.agents[0].to_learn = True
-w.agents[0].epsilon = 0.3
-w.agents[0].delta = 1-5e-6
+w.agents[0].load('shared_models/DQN52794.h5')
+w.agents[0].to_learn = False
+w.agents[0].epsilon = 1
+w.agents[0].delta = 1
 w.agents[0].skip = 5
 #w.agents[1].load('saved/DQN51100.h5')
 #w.agents[1].to_learn = False
